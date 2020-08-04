ff4592f71d199f961b998a05c0e4d8800d7a8068
czp-qcc <18801016449@163.com>
2020-08-04 15:31:35 +0800

    添加数据上报Excel预览功能

---
+++ src/views/dataReport/components/iframe.vue
@@ -0,0 +1,26 @@
+<!--  -->
+<template>
+  <div>
+    <iframe :src="'https://view.officeapps.live.com/op/view.aspx?src='+$route.query.url" frameborder="0" style="width:100%;height:100vh;"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
